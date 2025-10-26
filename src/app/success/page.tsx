7493ac5fbb1304540639a4a9055b68a04be9e358
Kaif Khan <kaif.khan1834@gmail.com>
2025-10-26 14:48:42 +0000

    Try fixing this error: `Console Error: `legacyBehavior` is deprecated an

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -10,10 +10,8 @@ export default function SuccessPage() {
         <p className="text-lg mb-8">
           Our agent will scour the internet to find the most suitable individual who can solve your problem. We'll be in touch.
         </p>
-        <Link href="/" legacyBehavior>
-          <a className="inline-block border border-black px-6 py-2 bg-white text-black hover:bg-gray-100">
-            Submit Another Problem
-          </a>
+        <Link href="/" className="inline-block border border-black px-6 py-2 bg-white text-black hover:bg-gray-100">
+          Submit Another Problem
         </Link>
       </div>
     </main>
