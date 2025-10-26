71cade9a474b9ffcfff5704e9ccc539648b0b493
Kaif Khan <kaif.khan1834@gmail.com>
2025-10-26 15:03:20 +0000

    make the image bigger with rounded corneres adn chagne up the contetn i

--- src/app/thank-you/page.tsx
+++ src/app/thank-you/page.tsx
@@ -6,20 +6,20 @@ import Image from "next/image";
 export default function ThankYouPage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-white p-4 font-serif">
-      <div className="relative w-full max-w-2xl">
+      <div className="relative w-full max-w-3xl">
         <Image
           src="https://images.pexels.com/photos/17483850/pexels-photo-17483850.png"
           alt="Abstract background"
           width={800}
           height={600}
-          className="object-cover"
+          className="object-cover rounded-lg"
           data-ai-hint="futuristic abstract"
         />
-        <div className="absolute inset-0 flex items-center justify-center bg-black/30">
+        <div className="absolute inset-0 flex items-center justify-center bg-black/30 rounded-lg">
           <div className="max-w-xl bg-white p-8 text-black rounded-none">
-            <h1 className="mb-4 text-3xl">Transmission Received</h1>
+            <h1 className="mb-4 text-3xl">Signal Acquired. Processing...</h1>
             <p className="text-lg">
-              Your query has been submitted to our talent orchestration engine. Our specialized AI is now executing a multi-vector analysis across our proprietary network of elite problem-solvers. We will be in touch shortly with a curated selection of candidates.
+              Your problem has been ingested by our cognitive engine. We are running multi-dimensional analysis against our network of elite strategists and engineers. A curated shortlist of problem-solvers is being assembled. Stand by for transmission.
             </p>
           </div>
         </div>
