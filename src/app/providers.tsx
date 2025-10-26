428c1c3d0ad5623fc1ebdb65c49242dc0450fac1
Kaif Khan <kaif.khan1834@gmail.com>
2025-10-26 16:53:08 +0000

    this project id add it twaztmnjmm

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,11 +1,13 @@
 'use client'
 import posthog from 'posthog-js'
 import { PostHogProvider } from 'posthog-js/react'
+import { clarity } from 'react-microsoft-clarity';
 
 if (typeof window !== 'undefined') {
   posthog.init('phc_WmQnO2rbuudmSRwtP2mgIUXcW4dP3d1f7Gz9LEzQ3YH', {
     api_host: 'https://us.posthog.com',
   });
+  clarity.init('twaztmnjmm');
 }
 export function PHProvider({ children }: { children: React.ReactNode }) {
   return <PostHogProvider client={posthog}>{children}</PostHogProvider>
