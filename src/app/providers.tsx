9f16975b71721888abe899f55ed0564d2cadb142
Kaif Khan <kaif.khan1834@gmail.com>
2025-10-26 16:51:56 +0000

    lets also add microsoft clarity what say (@microsoft/clarity Microsoft C

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -5,7 +5,7 @@ import { PostHogProvider } from 'posthog-js/react'
 if (typeof window !== 'undefined') {
   posthog.init('phc_WmQnO2rbuudmSRwtP2mgIUXcW4dP3d1f7Gz9LEzQ3YH', {
     api_host: 'https://us.posthog.com',
-  })
+  });
 }
 export function PHProvider({ children }: { children: React.ReactNode }) {
   return <PostHogProvider client={posthog}>{children}</PostHogProvider>
