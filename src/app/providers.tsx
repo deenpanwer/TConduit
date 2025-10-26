8167065254f1372133d6603fb088515f7ae7e070
Kaif Khan <kaif.khan1834@gmail.com>
2025-10-26 17:19:59 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve '@m

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -7,7 +7,7 @@ if (typeof window !== 'undefined') {
   posthog.init('phc_WmQnO2rbuudmSRwtP2mgIUXcW4dP3d1f7Gz9LEzQ3YH', {
     api_host: 'https://us.posthog.com',
   });
-  clarity.init('twaztmnjmm');
+  // clarity.init('twaztmnjmm');
 }
 export function PHProvider({ children }: { children: React.ReactNode }) {
   return <PostHogProvider client={posthog}>{children}</PostHogProvider>
