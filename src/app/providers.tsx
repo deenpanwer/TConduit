79a770948ac52dd684f0272730f78e358b56860b
Kaif Khan <kaif.khan1834@gmail.com>
2025-10-26 16:54:52 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve 're

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,7 +1,7 @@
 'use client'
 import posthog from 'posthog-js'
 import { PostHogProvider } from 'posthog-js/react'
-import { clarity } from 'react-microsoft-clarity';
+import clarity from '@microsoft/clarity';
 
 if (typeof window !== 'undefined') {
   posthog.init('phc_WmQnO2rbuudmSRwtP2mgIUXcW4dP3d1f7Gz9LEzQ3YH', {
