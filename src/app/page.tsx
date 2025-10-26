e89f48852ba475228a4407b6aaf55893074370c9
Kaif Khan <kaif.khan1834@gmail.com>
2025-10-26 11:41:50 +0000

    theres only one feild in the db thats named "input"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
           "Accept": "application/json",
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ data: [{ entry: inputValue }] }),
+        body: JSON.stringify({ data: [{ input: inputValue }] }),
       });
 
       if (!response.ok) {
